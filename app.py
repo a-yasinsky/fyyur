545a644393d50bb36549560e6410569e7502437f
a-yasinsky <anatoly@yasinsky.pro>
2019-10-08 08:56:48 +0600

    Venue's and Artist addition added

--- app.py
+++ app.py
@@ -287,54 +287,66 @@ def show_artist(artist_id):
 #  ----------------------------------------------------------------
 @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
-  form = ArtistForm()
-  artist={
-    "id": 4,
-    "name": "Guns N Petals",
-    "genres": ["Rock n Roll"],
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "326-123-5000",
-    "website": "https://www.gunsnpetalsband.com",
-    "facebook_link": "https://www.facebook.com/GunsNPetals",
-    "seeking_venue": True,
-    "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-    "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
-  }
   # TODO: populate form with fields from artist with ID <artist_id>
+  artist = Artist.query.get_or_404(artist_id)
+  form = ArtistForm(obj=artist)
   return render_template('forms/edit_artist.html', form=form, artist=artist)
 
 @app.route('/artists/<int:artist_id>/edit', methods=['POST'])
 def edit_artist_submission(artist_id):
   # TODO: take values from the form submitted, and update existing
   # artist record with ID <artist_id> using the new attributes
-
+   form = ArtistForm()
+   if form.validate_on_submit():
+       try:
+           artist = Artist()
+           form.populate_obj(artist)
+           db.session.commit()
+           flash('Artist ' + request.form['name'] + ' was successfully updated!')
+       except:
+           db.session.rollback()
+           print(sys.exc_info())
+           flash('Unable to write data!')
+       finally:
+           db.session.close()
+   else:
+       flash('Check your data!')
   return redirect(url_for('show_artist', artist_id=artist_id))
 
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
-  form = VenueForm()
-  venue={
-    "id": 1,
-    "name": "The Musical Hop",
-    "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-    "address": "1015 Folsom Street",
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "123-123-1234",
-    "website": "https://www.themusicalhop.com",
-    "facebook_link": "https://www.facebook.com/TheMusicalHop",
-    "seeking_talent": True,
-    "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-  }
   # TODO: populate form with values from venue with ID <venue_id>
+  venue = Venue.query.get_or_404(venue_id)
+  form = VenueForm(obj=venue)
   return render_template('forms/edit_venue.html', form=form, venue=venue)
 
 @app.route('/venues/<int:venue_id>/edit', methods=['POST'])
 def edit_venue_submission(venue_id):
   # TODO: take values from the form submitted, and update existing
   # venue record with ID <venue_id> using the new attributes
+  form = VenueForm()
+  if form.validate_on_submit():
+      # on successful db insert, flash success
+      error = False
+      data = {}
+      try:
+          venue = Venue()
+          form.populate_obj(venue)
+          #raise Exception('cheking the error hadling')
+          db.session.commit()
+          data['name'] = venue.name
+      except:
+          error = True
+          db.session.rollback()
+          print(sys.exc_info())
+          flash('Unable to write data!')
+      finally:
+          db.session.close()
+
+      if not error:
+          flash('Venue ' + data['name'] + ' was successfully updated!')
+  else:
+      flash('Check your data!')
   return redirect(url_for('show_venue', venue_id=venue_id))
 
 #  Create Artist
@@ -351,13 +363,14 @@ def create_artist_submission():
   # TODO: insert form data as a new Venue record in the db, instead
   # TODO: modify data to be the data object returned from db insertion
   form = ArtistForm()
+  data = {}
   if form.validate_on_submit():
       try:
           artist = Artist()
           form.populate_obj(artist)
           db.session.add(artist)
           db.session.commit()
-          flash('Artist ' + request.form['name'] + ' was successfully listed!')
+          data['name'] = artist.name
       except:
           db.session.rollback()
           print(sys.exc_info())
@@ -366,6 +379,7 @@ def create_artist_submission():
       finally:
           db.session.close()
 
+      flash('Artist ' + data['name'] + ' was successfully listed!')
       return redirect(url_for('artists'))
   else:
       flash('Check your data!')
